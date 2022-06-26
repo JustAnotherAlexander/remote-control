b20d0559d8758ebd7830e7843c9ae6bebbcf9fb5
Aleksandr <aleksandreliuz@gmail.com>
2022-06-26 23:11:20 +0300

    fix: correct result of mouse_position function

--- position.js
+++ position.js
@@ -2,5 +2,5 @@ import robot from 'robotjs';
 
 export function mouse_position() {
     let currentMousePosition = robot.getMousePos();
-    return `mouse_position ${currentMousePosition.x} px,${currentMousePosition.y} px`;
+    return `mouse_position ${currentMousePosition.x}px,${currentMousePosition.y}px`;
 }
